b9a5426071fe1eea40e22d77e453e8099d7cb859
amirsagiv83 <amirsagiv15@gmail.com>
2016-12-08 16:02:32 +0200

    Revert "Added an auxilary class to convert Psi to string and backwards #27"
    This reverts commit 7d1f2a6a1cbdc54e2bcab41e4ec1c50398e2ca57.

--- src/auxilary_layer/PsiRewrite.java
+++ src/auxilary_layer/PsiRewrite.java
@@ -4,14 +4,12 @@ import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiFileFactory;
 
 /**
  * Created by maorroey on 12/3/2016.
  */
 public class PsiRewrite {
-    public PsiElementFactory elementFactory;
-    public PsiFileFactory fileFactory;
+    public PsiElementFactory factory;
     public PsiClass psiClass;
 
     public void replace(PsiElement element1, PsiElement element2){
@@ -22,9 +20,5 @@ public class PsiRewrite {
             }
 
         }.execute();
-    }
-
-    public PsiFileFactory getFileFactory(){
-        return fileFactory;
     }
 }
