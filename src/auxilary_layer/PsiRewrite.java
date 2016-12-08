93dcadab8a256e3615a1d1f45db1d7237233f512
amirsagiv83 <amirsagiv15@gmail.com>
2016-12-08 19:00:42 +0200

    reupoading the string to Psi and backwards converter #27

--- src/auxilary_layer/PsiRewrite.java
+++ src/auxilary_layer/PsiRewrite.java
@@ -1,15 +1,14 @@
 package auxilary_layer;
 
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementFactory;
-import com.intellij.psi.PsiClass;
+import com.intellij.psi.*;
 
 /**
  * Created by maorroey on 12/3/2016.
  */
 public class PsiRewrite {
-    public PsiElementFactory factory;
+    public PsiElementFactory elementFactory;
+    public PsiFileFactory fileFactory;
     public PsiClass psiClass;
 
     public void replace(PsiElement element1, PsiElement element2){
@@ -20,5 +19,9 @@ public class PsiRewrite {
             }
 
         }.execute();
+    }
+
+    public PsiFileFactory getFileFactory(){
+        return fileFactory;
     }
 }
