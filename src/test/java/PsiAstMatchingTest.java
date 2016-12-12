d1ec6f5ca30858024db97c9cb43c7ea7f72b8ce0
Roei-m <m.roei.raz@gmail.com>
2016-12-12 22:48:41 +0200

    minor changes

--- src/test/java/PsiAstMatchingTest.java
+++ src/test/java/PsiAstMatchingTest.java
@@ -1,22 +1,20 @@
-import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.psi.PsiFile;
-import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import com.intellij.testFramework.PsiTestCase;
 
-//TODO @RoeiRaz finish writing this
 //TODO @RoeiRaz We are required to use JUnit3 here. check if there is a way to use JUnit4.
 
 /**
  * @author RoeiRaz
  */
-public class PsiAstMatchingTest extends LightCodeInsightFixtureTestCase {
+public class PsiAstMatchingTest extends PsiTestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
     }
 
-    public void testA() {
-        String src = "class A {}";
-        PsiFile file = myFixture.configureByText(StdFileTypes.JAVA, src);
+    public void testA() throws Exception {
+        // TODO @amirsagiv83 this is how you can create PsiFile without 'running' intellij.
+        PsiFile file = createFile("foo.java", "class A{}");
         System.out.println(file.getText());
     }
 }
