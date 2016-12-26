b35bf9838a698140519c4c1fea7442977931ec8a
maoritsius <maor.roey@gmail.com>
2016-12-26 13:54:09 +0200

    AnyMatches class skeleton #49

---
+++ src/plugin/tippers/AnyMatch.java
@@ -0,0 +1,34 @@
+package plugin.tippers;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiForStatement;
+import plugin.tipping.Tipper;
+import plugin.tipping.Tip;
+import plugin.tipping.TipperCategory;
+import auxilary_layer.PsiRewrite;
+import java.util.Collection;
+
+/**
+ * Created by maorroey on 12/26/2016.
+ */
+public class AnyMatch extends NanoPatternTipper<PsiForStatement>{
+
+
+    public boolean canTip(PsiElement ¢){
+        return false;
+    }
+
+    @Override
+    public String description(PsiForStatement ¢) {
+        return "Replaces AnyMatch-for statement with the appropriate .stream().AnyMatch Java Collection function call";
+    }
+
+    @Override
+    protected Tip pattern(final PsiForStatement ¢){
+        return tip(¢);
+    }
+
+    @Override
+    public Class<PsiForStatement> getPsiClass() {
+        return PsiForStatement.class;
+    }
+}
