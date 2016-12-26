712962cdb5ef18614e0a71fd7041ddf6ba6d97b0
Anna Belozovsky <anechka2005@gmail.com>
2016-12-26 14:41:26 +0200

    canTip added #48

--- src/plugin/tippers/LispLastElement.java
+++ src/plugin/tippers/LispLastElement.java
@@ -1,9 +1,13 @@
 package plugin.tippers;
 
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiMethodCallExpression;
+import auxilary_layer.az;
+import auxilary_layer.iz;
+import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
 import plugin.tipping.Tip;
 import plugin.tipping.Tipper;
+
+import java.util.LinkedList;
 
 /**
  * @author AnnaBel7
@@ -12,19 +16,55 @@ import plugin.tipping.Tipper;
 
 public class LispLastElement implements Tipper<PsiMethodCallExpression> {
 
-
     private boolean canTip(PsiMethodCallExpression e) {
-        return false;
+        // holds $x
+        PsiReferenceExpression[] outterReference = PsiTreeUtil.getChildrenOfType(e.getMethodExpression(), PsiReferenceExpression.class);
+        // holds "get"
+        PsiIdentifier[] outterIdentifier = PsiTreeUtil.getChildrenOfType(e.getMethodExpression(), PsiIdentifier.class);
+        //checks the call is to $x.get
+        if (outterReference.length != 1 || outterIdentifier.length != 1 || !outterIdentifier[0].getText().equals("get")) {
+            return false;
+        }
+
+        // holds the arguments
+        PsiExpression[] arguments = e.getArgumentList().getExpressions();
+        // checks there is only one argument and it is a binary expression
+        if (arguments.length != 1 || !isBinaryExpression(arguments[0])) {
+            return false;
+        }
+
+        //holds $x
+        PsiReferenceExpression[] innerReference = PsiTreeUtil.getChildrenOfType(((PsiMethodCallExpression)((PsiBinaryExpression)arguments[0]).getLOperand()).getMethodExpression(), PsiReferenceExpression.class);
+        //holds "size"
+        PsiIdentifier[] innerIdentifier = PsiTreeUtil.getChildrenOfType(((PsiMethodCallExpression)((PsiBinaryExpression)arguments[0]).getLOperand()).getMethodExpression(), PsiIdentifier.class);
+        // checks that the left operand of the binary expression is $x.size
+        if(!outterReference[0].getText().equals(innerReference[0].getText()) || !innerIdentifier[0].getText().equals("size")){
+            System.out.println("third");
+            return false;
+        }
+        // checks minus 1
+        System.out.println("return");
+        return ((PsiBinaryExpression)arguments[0]).getROperand().getText().equals("1")
+                && ((PsiBinaryExpression)arguments[0]).getOperationSign().getText().equals("-");
+    }
+
+    LinkedList<Integer> l = new LinkedList<>();
+
+    private Integer check() {
+        return l.get(l.size() - 1);
     }
 
     @Override
     public boolean canTip(PsiElement e) {
-        return e instanceof PsiMethodCallExpression && canTip((PsiMethodCallExpression) e);
+        if(isCallExpression(e)){
+            System.out.println(e.getText());
+        }
+        return isCallExpression(e) && canTip((PsiMethodCallExpression) e);
     }
 
     @Override
     public String description(PsiMethodCallExpression psiMethodCallExpression) {
-        return null;
+        return "Lisp last element";
     }
 
     @Override
@@ -35,5 +75,13 @@ public class LispLastElement implements Tipper<PsiMethodCallExpression> {
     @Override
     public Class<PsiMethodCallExpression> getPsiClass() {
         return PsiMethodCallExpression.class;
+    }
+
+    private boolean isCallExpression(PsiElement e) {
+        return e instanceof PsiMethodCallExpression;
+    }
+
+    private boolean isBinaryExpression(PsiElement e) {
+        return e instanceof PsiBinaryExpression;
     }
 }
