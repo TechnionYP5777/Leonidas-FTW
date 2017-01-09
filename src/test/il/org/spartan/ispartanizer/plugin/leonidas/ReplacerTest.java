89baaadc44b2b8b622f94249dacb87b3c29dd4dd
Anna Belozovsky <anechka2005@gmail.com>
2017-01-09 22:55:09 +0200

    replacer test file added

---
+++ src/test/il/org/spartan/ispartanizer/plugin/leonidas/ReplacerTest.java
@@ -0,0 +1,22 @@
+package il.org.spartan.ispartanizer.plugin.leonidas;
+
+import il.org.spartan.ispartanizer.tippers.TipperTest;
+import junit.framework.TestCase;
+
+/**
+ * @author AnnaBel7
+ * @since 09/01/2017.
+ */
+public class ReplacerTest extends TipperTest {
+    public void testReplace1() {
+
+    }
+
+    public void testReplace2() {
+
+    }
+
+    public void testReplace3() {
+
+    }
+}
