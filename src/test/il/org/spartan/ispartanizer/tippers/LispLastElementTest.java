4abadf00a1ce9339fe696a7334906712d4bbd267
Anna Belozovsky <anechka2005@gmail.com>
2017-01-13 13:49:47 +0200

    added createReplacement tests #77

--- src/test/il/org/spartan/ispartanizer/tippers/LispLastElementTest.java
+++ src/test/il/org/spartan/ispartanizer/tippers/LispLastElementTest.java
@@ -1,5 +1,6 @@
 package il.org.spartan.ispartanizer.tippers;
 
+import com.intellij.psi.PsiMethodCallExpression;
 import il.org.spartan.ispartanizer.plugin.tippers.LispLastElement;
 import il.org.spartan.ispartanizer.tippers.TipperTest;
 import junit.framework.TestCase;
@@ -10,26 +11,26 @@ import junit.framework.TestCase;
  */
 public class LispLastElementTest extends TipperTest {
     public void testCanTipLegal() {
-        assertEquals(true, new LispLastElement().canTip(createTestExpressionFromString("l.get(l.size()-1)")));
+        assertTrue(new LispLastElement().canTip(createTestExpressionFromString("l.get(l.size()-1)")));
     }
 
     public void testCanTipIllegal1() {
-        assertEquals(false, new LispLastElement().canTip(createTestExpressionFromString("x.get(y.size()-1)")));
+        assertFalse(new LispLastElement().canTip(createTestExpressionFromString("x.get(y.size()-1)")));
     }
 
     public void testCanTipIllegal2() {
-        assertEquals(false, new LispLastElement().canTip(createTestExpressionFromString("x = l.get(l.size()-1)")));
+        assertFalse(new LispLastElement().canTip(createTestExpressionFromString("x = l.get(l.size()-1)")));
     }
 
     public void testCreateReplacement1() {
-
+        PsiMethodCallExpression methodCallExpression = createTestMethodCallExpression("l.get", "l.size()-1");
+        assertTrue(new LispLastElement().canTip(methodCallExpression));
+        assertEquals("last(l)", new LispLastElement().createReplacement(methodCallExpression).getText());
     }
 
     public void testCreateReplacement2() {
-
-    }
-
-    public void testCreateReplacement3() {
-
+        PsiMethodCallExpression methodCallExpression = createTestMethodCallExpression("x.get", "x.size()-1");
+        assertTrue(new LispLastElement().canTip(methodCallExpression));
+        assertEquals("last(x)", new LispLastElement().createReplacement(methodCallExpression).getText());
     }
 }
