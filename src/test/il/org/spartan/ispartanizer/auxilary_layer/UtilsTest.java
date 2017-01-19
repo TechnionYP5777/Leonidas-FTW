0884c17607dc555f5768cd190c59a681468e5a3b
maoritsius <maor.roey@gmail.com>
2017-01-19 16:46:57 +0200

    findClass #86

--- src/test/il/org/spartan/ispartanizer/auxilary_layer/UtilsTest.java
+++ src/test/il/org/spartan/ispartanizer/auxilary_layer/UtilsTest.java
@@ -1,6 +1,8 @@
 package il.org.spartan.ispartanizer.auxilary_layer;
 
+import com.intellij.psi.*;
 import com.intellij.testFramework.PsiTestCase;
+import il.org.spartan.ispartanizer.tippers.TipperTest;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,7 +12,8 @@ import java.util.List;
  * @author Roey Maor, michal cohen
  * @since 12/22/2016.
  */
-public class UtilsTest extends PsiTestCase {
+public class UtilsTest extends TipperTest {
+    static final String dummyFileName = "test.java";
 
     public void testin() throws Exception {
         List<Integer> aList = new ArrayList<>();
@@ -25,6 +28,29 @@ public class UtilsTest extends PsiTestCase {
     }
 
     public void testfindClass() throws Exception {
+        /*
+            the assert statement is used as a latch to check if it is in a parent class or not
+         */
+        String classExistsSource1 = "package test; "+
+                "public class Test { "+
+                "public Test() { "+
+                "final int x=3; assert(true); System.out.println(\"lalala\"); "+
+                "} "+
+                "}";
+
+        PsiFile classExistsFile1 = createFile(dummyFileName,classExistsSource1);
+        PsiAssertStatement assertInSource1 = Utils.getChildrenOfType(classExistsFile1, PsiAssertStatement.class).get(0);
+
+        String classExistsSource2 =
+                "public Test() { "+
+                "final int x=3; assert(true); System.out.println(\"lalala\"); "+
+                "} ";
+
+        PsiMethod methodIn2 = createTestMethodFromString(classExistsSource2);
+
+        assertTrue(Utils.findClass(assertInSource1) != null);
+        assertTrue(Utils.findClass(assertInSource1).getName().equals("Test"));
+        assertTrue(Utils.findClass(methodIn2) == null);
 
     }
 
