a506d1bad1d3201502f208b8edfbc628cde8c61e
Michal Cohen <cohenmelany1@gmail.com>
2017-01-21 10:39:54 +0200

    new Tipper

---
+++ resources/spartanizer/FutureLeonidasTippers/GreaterConditionToSmaller.java
@@ -0,0 +1,28 @@
+package spartanizer.LeonidasTippers;
+
+import com.intellij.psi.PsiIfStatement;
+import il.org.spartan.ispartanizer.plugin.leonidas.GenericPsiElementStub;
+import il.org.spartan.ispartanizer.plugin.leonidas.Leonidas;
+
+/**
+ * Replace if(e1 > e2); with if(e2 < e1)
+ *
+ * @author michalcohen
+ * @since 21-01-17
+ */
+public class GreaterConditionToSmaller extends GenericPsiElementStub {
+
+    @Leonidas(PsiIfStatement.class)
+    public void from() {
+        if (booleanExpression(0) > booleanExpression(1))
+            anyBlock(2);
+
+    }
+
+    @Leonidas(PsiIfStatement.class)
+    public void to(){
+        if(booleanExpression(1) < booleanExpression(0))
+            anyBlock(2);
+
+    }
+}
