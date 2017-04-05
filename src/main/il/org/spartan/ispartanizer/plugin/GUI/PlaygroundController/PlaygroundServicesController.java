c5c5d51604ae061236d337aaa47ad130c73e2140
Anna Belozovsky <anechka2005@gmail.com>
2017-04-06 00:59:02 +0300

    fixed sizes and added ids for elements #160

--- src/main/il/org/spartan/ispartanizer/plugin/GUI/PlaygroundController/PlaygroundServicesController.java
+++ src/main/il/org/spartan/ispartanizer/plugin/GUI/PlaygroundController/PlaygroundServicesController.java
@@ -1,6 +1,9 @@
 package il.org.spartan.ispartanizer.plugin.GUI.PlaygroundController;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -8,12 +11,21 @@ import java.util.ResourceBundle;
 /**
  * @author Anna Belozovsky
  * @since 05/04/2017
- *
+ * <p>
  * This is a controller class for the Playground GUI
  */
 public class PlaygroundServicesController implements Initializable {
+
+    public TextArea inputCode;
+    public Text outputCode;
+    public Button spartanizeButton;
+    public Button clearButton;
+
+    /**
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
     }
 }
