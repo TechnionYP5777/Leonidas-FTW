03a4c1046701c81e7ec93c8efd2c97218c4c8ce7
Anna Belozovsky <anechka2005@gmail.com>
2017-04-05 15:58:59 +0300

    started working on playground gui #160

---
+++ src/main/il/org/spartan/ispartanizer/plugin/GUI/PlaygroundController/PlaygroundServicesController.java
@@ -0,0 +1,19 @@
+package il.org.spartan.ispartanizer.plugin.GUI.PlaygroundController;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author Anna Belozovsky
+ * @since 05/04/2017
+ *
+ * This is a controller class for the Playground GUI
+ */
+public class PlaygroundServicesController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
