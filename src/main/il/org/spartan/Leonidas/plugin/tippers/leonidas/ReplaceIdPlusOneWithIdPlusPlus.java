7cc8703fba7e517c8ca5b33bf35ab3d9a34ffc96
Sharon <sharonkl@campus.technion.ac.il>
2017-05-27 13:58:05 +0300

    Add a new tipper to test the Identifier basic block
    Tests have been added to test this tipper.

---
+++ src/main/il/org/spartan/Leonidas/plugin/tippers/leonidas/ReplaceIdPlusOneWithIdPlusPlus.java
@@ -0,0 +1,26 @@
+package il.org.spartan.Leonidas.plugin.tippers.leonidas;
+
+/**
+ * @author Sharon LK
+ */
+public class ReplaceIdPlusOneWithIdPlusPlus implements LeonidasTipperDefinition {
+    int identifier0;
+
+    @Override
+    public void matcher() {
+        new Template(() -> {
+            /** start */
+            identifier0 += 1;
+            /** end */
+        });
+    }
+
+    @Override
+    public void replacer() {
+        new Template(() -> {
+            /** start */
+            identifier0++;
+            /** end */
+        });
+    }
+}
