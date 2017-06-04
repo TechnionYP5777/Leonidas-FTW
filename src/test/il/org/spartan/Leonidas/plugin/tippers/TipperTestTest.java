2cc09a453beba5500878d778fc19f6790e367ad2
RoeyMaor <maor.roey@gmail.com>
2017-06-04 08:31:58 +0300

    remove prints from tests

--- src/test/il/org/spartan/Leonidas/plugin/tippers/TipperTestTest.java
+++ src/test/il/org/spartan/Leonidas/plugin/tippers/TipperTestTest.java
@@ -1,7 +1,5 @@
 package il.org.spartan.Leonidas.plugin.tippers;
 import il.org.spartan.Leonidas.PsiTypeHelper;
-import il.org.spartan.Leonidas.plugin.tippers.leonidas.IfDoubleNot;
-import il.org.spartan.Leonidas.plugin.tippers.leonidas.IfEmptyThen;
 import org.junit.Test;
 
 
@@ -19,7 +17,7 @@ public class TipperTestTest extends PsiTypeHelper {
     @Test
     public void testTipperTest(){
         SafeReference idn = new SafeReference();
-        TipperTest ts = new TipperTest(idn,this,true);
+        TipperTest ts = new TipperTest(idn,this,false);
         ts.check();
     }
 
