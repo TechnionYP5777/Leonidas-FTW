49a0b0e6c39c9f8fdb3ee36561d829ea40668cf1
Anna Belozovsky <anechka2005@gmail.com>
2017-06-18 18:50:37 +0300

    very basic version of #1364 finished

--- src/main/il/org/spartan/Leonidas/plugin/tippers/leonidas/UnusedParameterToUnderscore.java
+++ src/main/il/org/spartan/Leonidas/plugin/tippers/leonidas/UnusedParameterToUnderscore.java
@@ -1,23 +1,19 @@
 package il.org.spartan.Leonidas.plugin.tippers.leonidas;
 
-import il.org.spartan.Leonidas.plugin.tippers.leonidas.LeonidasTipperDefinition.TipperUnderConstruction;
-
 import java.util.HashMap;
 import java.util.Map;
 
 import static il.org.spartan.Leonidas.plugin.leonidas.BasicBlocks.GenericPsiElementStub.anyNumberOf;
 import static il.org.spartan.Leonidas.plugin.leonidas.BasicBlocks.GenericPsiElementStub.statement;
 import static il.org.spartan.Leonidas.plugin.leonidas.The.element;
-import static il.org.spartan.Leonidas.plugin.tippers.leonidas.LeonidasTipperDefinition.UnderConstructionReason.UNTESTED;
 
 /**
- * <Tipper description>
- * UnusedParameterToUnderscore
+ * change unused parameter to __
  *
  * @author Anna Belozovsky
  * @since 15/06/2017
  */
-@TipperUnderConstruction(UNTESTED)
+//@TipperUnderConstruction(UNTESTED)
 public class UnusedParameterToUnderscore implements LeonidasTipperDefinition {
 
     /**
@@ -28,6 +24,7 @@ public class UnusedParameterToUnderscore implements LeonidasTipperDefinition {
     @Override
     public void constraints() {
         element(4).asStatement.mustNotRefer(3);
+        element(3).asIdentifier.notContains("__");
     }
 
     @Override
@@ -63,7 +60,7 @@ public class UnusedParameterToUnderscore implements LeonidasTipperDefinition {
     @Override
     public Map<String, String> getExamples() {
         Map<String, String> examples = new HashMap<>();
-
+        examples.put("int foo(int x){\n\treturn 2;\n}", "int foo(int __){\n\treturn 2;\n}");
         return examples;
     }
 
