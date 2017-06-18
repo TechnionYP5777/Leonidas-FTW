604af1d212d655177f699a4592005b6c047d13f0
RoeiRaz <m.roei.raz@gmail.com>
2017-06-18 11:02:23 +0300

    created ActionMonitor skeleton
    Signed-off-by: RoeiRaz <m.roei.raz@gmail.com>

---
+++ src/main/il/org/spartan/Leonidas/plugin/GUI/ActionMonitor/ActionMonitor.java
@@ -0,0 +1,13 @@
+package il.org.spartan.Leonidas.plugin.GUI.ActionMonitor;
+
+import javax.swing.*;
+
+/**
+ * @author RoeiRaz
+ * @since 18/06/17
+ */
+public class ActionMonitor {
+    private JPanel mainPanel;
+    private JProgressBar progressBar;
+    private JTextArea OutputConsole;
+}
