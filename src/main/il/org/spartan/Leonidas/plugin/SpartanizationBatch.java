11e445035abc9422d7a4cf295c6661f8d4627d03
RoeiRaz <m.roei.raz@gmail.com>
2017-06-23 15:20:10 +0300

    refactored and simplified the spartanize project option
    Signed-off-by: RoeiRaz <m.roei.raz@gmail.com>

--- src/main/il/org/spartan/Leonidas/plugin/SpartanizationBatch.java
+++ src/main/il/org/spartan/Leonidas/plugin/SpartanizationBatch.java
@@ -1,88 +1,51 @@
 package il.org.spartan.Leonidas.plugin;
 
-import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.JavaRecursiveElementVisitor;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import il.org.spartan.Leonidas.auxilary_layer.PsiRewrite;
-import org.apache.commons.lang.math.RandomUtils;
+import il.org.spartan.Leonidas.auxilary_layer.Wrapper;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * @author RoeiRaz
  * @since 18/06/17
  * <p>
  * SpartanizationBatch is a collection of spartanization work instructions
- * that can be invoked in the future. Should LOCK the given PSI element.
+ * that can be invoked in the future.
  */
-public class SpartanizationBatch {
+public class SpartanizationBatch extends Task.Modal {
 
-    private static final String WRITE_COMMAND_NAME = SpartanizationBatch.class.getSimpleName()
-            + RandomUtils.nextInt();
-
+    private final static String title = "Spartanization in progress";
     private final Project project;
-    private final Set<PsiElement> elements;
     private final Set<PsiFile> files;
-    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-    private final Optional<SpartanizerBatchListener> listenerOptional;
-    private final WriteCommandAction commandAction;
 
-    SpartanizationBatch(Project project, Set<? extends PsiElement> elements) {
-        this(project, elements, null);
-    }
-
-    SpartanizationBatch(Project project, Set<? extends PsiElement> elements, SpartanizerBatchListener listener) {
+    SpartanizationBatch(Project project, Set<PsiFile> files) {
+        super(project, title, true);
         this.project = project;
-        this.elements = new HashSet<>(elements);
-        this.listenerOptional = Optional.ofNullable(listener);
-        this.files = elements.stream().map(PsiElement::getContainingFile).collect(Collectors.toSet());
-        this.commandAction = new RunCommand(project, WRITE_COMMAND_NAME, files.stream().toArray(PsiFile[]::new));
-
+        this.files = files;
     }
 
-    public void invoke() {
-        commandAction.execute();
-    }
-
-    public Project getProject() {
-        return project;
-    }
-
-    public Set<PsiElement> getElements() {
-        return elements;
-    }
-
-    private class RunCommand extends WriteCommandAction.Simple {
-        RunCommand(Project project, String commandName, PsiFile... files) {
-            super(project, commandName, files);
-        }
-
-        @Override
-        protected void run() throws Throwable {
-            getElements().forEach(e -> {
-                if (Toolbox.getInstance().checkExcluded(e.getContainingFile()))
-                    return;
-                e.accept(new JavaRecursiveElementVisitor() {
-                    @Override
-                    public void visitElement(PsiElement ce) {
-                        super.visitElement(ce);
-                        if (!Toolbox.getInstance().canTip(ce))
-                            return;
-                        Toolbox.getInstance().getAllTippers().forEach(t -> {
-                            if (!t.canTip(ce))
-                                return;
-                            //noinspection unchecked
-                            t.tip(ce).go(new PsiRewrite().project(project).psiFile(e.getContainingFile()));
-                        });
-                    }
-                });
-
+    @Override
+    public void run(@NotNull ProgressIndicator indicator) {
+        Wrapper<Integer> i = new Wrapper<>(0);
+        files.forEach(f -> {
+            // It's important to run this with Application::invokeLater and with
+            // the default modality - this way we have write access to the model
+            // through the EDT from which this task was generated.
+            ApplicationManager.getApplication().invokeLater(() -> {
+                Spartanizer.spartanizeFileRecursively(f);
             });
-        }
+            i.set(i.get() + 1);
+            indicator.setFraction((double) i.get() / files.size());
+        });
+    }
+
+
+    public Set<PsiFile> getFiles() {
+        return files;
     }
 }
--- src/main/il/org/spartan/Leonidas/plugin/SpartanizeProjectAction.java
+++ src/main/il/org/spartan/Leonidas/plugin/SpartanizeProjectAction.java
@@ -1,15 +1,21 @@
 package il.org.spartan.Leonidas.plugin;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.roots.ContentIterator;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.PsiManager;
+import icons.Icons;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,8 +26,10 @@ import java.util.Optional;
 public class SpartanizeProjectAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
+
         // Collect all project PsiFiles
         List<PsiFile> psiFiles = new ArrayList<>();
+
         ProjectFileIndex.SERVICE.getInstance(e.getProject()).iterateContent(new ContentIterator() {
             @Override
             public boolean processFile(VirtualFile fileOrDir) {
@@ -31,6 +39,13 @@ public class SpartanizeProjectAction extends AnAction {
             }
         });
 
-        // TODO @RoeiRaz
+        ProgressManager.getInstance().run(new SpartanizationBatch(e.getProject(), new HashSet<>(psiFiles)));
+        Notifications.Bus.notify(new Notification("il.org.spartan",
+                Icons.LeonidasSmall,
+                "Spartanizer",
+                "Finished spartanization of " + psiFiles.size() + " files.",
+                null,
+                NotificationType.INFORMATION,
+                null));
     }
 }
