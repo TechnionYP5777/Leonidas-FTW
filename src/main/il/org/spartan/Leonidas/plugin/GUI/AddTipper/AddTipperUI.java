a0d97939a534687c34bb43475c8893a6d41047ce
Sharon <sharonkl@campus.technion.ac.il>
2017-06-26 01:18:42 +0300

    Remove the advertisement text #1510

--- src/main/il/org/spartan/Leonidas/plugin/GUI/AddTipper/AddTipperUI.java
+++ src/main/il/org/spartan/Leonidas/plugin/GUI/AddTipper/AddTipperUI.java
@@ -82,14 +82,6 @@ public class AddTipperUI extends JFrame {
 
         //======== panel4 ========
         {
-
-            // JFormDesigner evaluation mark
-            panel4.setBorder(new javax.swing.border.CompoundBorder(
-                new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-                    "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
-                    javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-                    java.awt.Color.red), panel4.getBorder())); panel4.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
-
             panel4.setLayout(new GridBagLayout());
             ((GridBagLayout)panel4.getLayout()).columnWidths = new int[] {0, 0, 0};
             ((GridBagLayout) panel4.getLayout()).rowHeights = new int[]{0, 0, 0};
