e7820e0685a3d6aba75fe581597862ee34fe6e75
amirsagiv83 <amirsagiv15@gmail.com>
2017-06-27 15:56:34 +0300

    Improved Leonidas Menu Design

--- src/main/icons/Icons.java
+++ src/main/icons/Icons.java
@@ -11,6 +11,9 @@ public enum Icons {
     public static Icon Playground = IconLoader.getIcon("/playground.png");
     public static Icon Info = IconLoader.getIcon("/info.png");
     public static Icon LeonidasSmall = IconLoader.getIcon("/leonidasSmall.png");
+    public static Icon LeonidasSmallPurple = IconLoader.getIcon("/leonidasSmallPurple.png");
+    public static Icon LeonidasSmallRed = IconLoader.getIcon("/leonidasSmallRed.png");
+    public static Icon LeonidasSmallGray = IconLoader.getIcon("/leonidasSmallGray.png");
     public static Icon Leonidas = IconLoader.getIcon("/leonidas.png");
     public static Icon Enable = IconLoader.getIcon("/enable.png");
     public static Icon Disable = IconLoader.getIcon("/disable.png");
