cc7e3de3b3203a8b64e58ba1a6d6af75f3a8ada8
Sharon <sharonkl@campus.technion.ac.il>
2017-06-29 16:45:01 +0300

    First implementation of the data saving mechanism

--- src/main/il/org/spartan/Leonidas/plugin/GUI/ToolBoxController/TipperDataManager.java
+++ src/main/il/org/spartan/Leonidas/plugin/GUI/ToolBoxController/TipperDataManager.java
@@ -3,10 +3,17 @@ package il.org.spartan.Leonidas.plugin.GUI.ToolBoxController;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import il.org.spartan.Leonidas.plugin.Toolbox;
+import il.org.spartan.Leonidas.plugin.UserControlled;
+import il.org.spartan.Leonidas.plugin.tippers.LeonidasTipper;
+import il.org.spartan.Leonidas.plugin.tipping.Tipper;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 /**
  * Created by Sharon on 29-Jun-17.
@@ -30,10 +37,38 @@ public class TipperDataManager implements PersistentStateComponent<TipperDataMan
     }
 
     public class TipperDataContainer {
-        public Map<String, Object> data = new HashMap<>();
+        // A mapping from tipper name to (a mapping from basic block id to (a mapping from field name to its value)))
+        public Map<String, Map<Integer, Map<String, Object>>> data = new HashMap<>();
 
         public TipperDataContainer() {
-            // TODO: Iterate over all tippers and get the data
+            List<Tipper> tippers = Toolbox.getInstance().getAllTippers();
+
+            // Iterate over all leonidas tippers
+            tippers.stream()
+                    .filter(tipper -> tipper instanceof LeonidasTipper)
+                    .map(tipper -> (LeonidasTipper) tipper)
+                    .forEach(tipper -> {
+                        data.put(tipper.name(), new HashMap<>());
+
+                        // Iterate over all generic blocks in the tipper
+                        Stream.concat(tipper.getMatcher().getAllRoots().stream(), tipper.getMatcher().getAllRoots().stream())
+                                .map(LeonidasTipper::getGenericElements)
+                                .flatMap(Collection::stream)
+                                .forEach(encapsulator -> {
+                                    data.get(tipper.name()).put(encapsulator.getId(), new HashMap<>());
+
+                                    // Iterate over all the fields in the generic block
+                                    Stream.of(encapsulator.getClass().getFields())
+                                            .filter(field -> field.isAnnotationPresent(UserControlled.class))
+                                            .forEach(field -> {
+                                                try {
+                                                    data.get(tipper.name()).get(encapsulator.getId()).put(field.getName(), field.get(encapsulator));
+                                                } catch (IllegalAccessException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            });
+                                });
+                    });
         }
     }
 }
