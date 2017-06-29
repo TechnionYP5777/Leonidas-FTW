6d2db6280913c661d3540f3694833c3d88e0a3a1
RoeyMaor <maor.roey@gmail.com>
2017-06-30 01:04:55 +0300

    tryFinallyEMptyTipper

---
+++ src/main/il/org/spartan/Leonidas/plugin/tippers/leonidas/TryFinallyEmptyRemove.java
@@ -0,0 +1,65 @@
+package il.org.spartan.Leonidas.plugin.tippers.leonidas;
+
+import il.org.spartan.Leonidas.auxilary_layer.ExampleMapFactory;
+
+import java.util.Map;
+
+import static il.org.spartan.Leonidas.plugin.leonidas.BasicBlocks.GenericPsiElementStub.*;
+
+
+/**
+ * remove empty finally
+ *
+ * @author Roey Maor
+ * @since 29/06/2017
+ */
+public class TryFinallyEmptyRemove implements LeonidasTipperDefinition{
+
+
+    @Override
+    public void matcher() {
+        new Template(() -> {
+            /*start*/
+            try{
+                anyNumberOf(statement(0));
+            } catch(Exception e){
+                anyNumberOf(statement(1));
+            } finally {
+            }
+            /*end*/
+
+        });
+    }
+
+    @Override
+    public void replacer() {
+        new Template(() -> {
+            /*start*/
+            try{
+                anyNumberOf(statement(0));
+            } catch(Exception e){
+                anyNumberOf(statement(1));
+            }
+            /*end*/
+        });
+    }
+
+    @Override
+    public Map<String, String> getExamples() {
+        return new ExampleMapFactory()
+                .put("try{\n" +
+                                "int x=5;\n" +
+                                "} catch(Exception e){\n" +
+                                "e.printStackTrace();\n" +
+                                "} finally {\n" +
+                                "}",
+                        "try{\n" +
+                                "int x = 5;\n" +
+                                "} catch(Exception e){\n" +
+                                "e.printStackTrace();\n" +
+                                "}")
+                .map();
+    }
+
+
+}
