ae59100b902b18d8433a60a21d62d881941cb77f
RoeyMaor <maor.roey@gmail.com>
2017-06-30 02:10:33 +0300

    added 2 more tippers

---
+++ src/main/il/org/spartan/Leonidas/plugin/tippers/leonidas/CastToLong2Multiply1L.java
@@ -0,0 +1,47 @@
+package il.org.spartan.Leonidas.plugin.tippers.leonidas;
+
+import il.org.spartan.Leonidas.auxilary_layer.ExampleMapFactory;
+
+import java.util.Map;
+
+import static il.org.spartan.Leonidas.plugin.leonidas.BasicBlocks.GenericPsiElementStub.*;
+
+
+/**
+ * remove empty finally
+ *
+ * @author Roey Maor
+ * @since 29/06/2017
+ */
+public class CastToLong2Multiply1L implements LeonidasTipperDefinition{
+
+    int identifier0;
+
+    @Override
+    public void matcher() {
+        new Template(() ->
+            /*start*/
+                (long)identifier0
+            /*end*/
+
+        );
+    }
+
+    @Override
+    public void replacer() {
+        new Template(() ->
+             /*start*/
+            1L*identifier0
+            /*end*/
+        );
+    }
+
+    @Override
+    public Map<String, String> getExamples() {
+        return new ExampleMapFactory()
+                .put("(long)X","1L*X")
+                .map();
+    }
+
+
+}
