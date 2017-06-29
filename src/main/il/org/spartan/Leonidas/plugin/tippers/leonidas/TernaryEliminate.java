901189d6a1bdcb43a2515f349167c1a8fe988ea2
amirsagiv83 <amirsagiv15@gmail.com>
2017-06-29 14:57:09 +0300

    New Tipper, Ternary Eliminate

---
+++ src/main/il/org/spartan/Leonidas/plugin/tippers/leonidas/TernaryEliminate.java
@@ -0,0 +1,43 @@
+package il.org.spartan.Leonidas.plugin.tippers.leonidas;
+
+import il.org.spartan.Leonidas.auxilary_layer.ExampleMapFactory;
+
+import java.util.Map;
+
+import static il.org.spartan.Leonidas.plugin.leonidas.BasicBlocks.GenericPsiElementStub.booleanExpression;
+import static il.org.spartan.Leonidas.plugin.leonidas.BasicBlocks.GenericPsiElementStub.expression;
+
+/**
+ * Trenary Eliminate
+ *
+ * @author Amir Sagiv
+ * @since 28-06-2017
+ */
+public class TernaryEliminate implements LeonidasTipperDefinition {
+
+
+    @Override
+    public void constraints() {
+    }
+
+    @Override
+    public void matcher() {
+        new Template(() ->/* start */ booleanExpression(0) ? expression(1) : expression(1)/* end */);
+    }
+
+    @Override
+    public void replacer() {
+        new Template(() -> /* start */ expression(1) /* end */);
+    }
+
+    @Override
+    public Map<String, String> getExamples() {
+        return new ExampleMapFactory()
+                .put("x == 9 ? 9:9", "9")
+                .put("true ? false : false", "false")
+                //.put("if(( x == a ? b : b) == b){}", "if ((b) == b) {}")
+                .put("true? true:false",null)
+                .map();
+    }
+
+}
