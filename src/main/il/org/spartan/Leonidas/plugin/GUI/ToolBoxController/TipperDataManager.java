c7c5ae8432ff939a540c3580e99cf68c6060a740
Sharon <sharonkl@campus.technion.ac.il>
2017-06-29 15:26:21 +0300

    Add a container class for the tippers data

--- src/main/il/org/spartan/Leonidas/plugin/GUI/ToolBoxController/TipperDataManager.java
+++ src/main/il/org/spartan/Leonidas/plugin/GUI/ToolBoxController/TipperDataManager.java
@@ -3,27 +3,37 @@ package il.org.spartan.Leonidas.plugin.GUI.ToolBoxController;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
-import com.intellij.util.xmlb.XmlSerializerUtil;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Sharon on 29-Jun-17.
  */
 @State(
-        name = "CustomLeonidasTippers",
+        name = "TipperDataContainer",
         storages = {
-                @Storage("CustomLeonidasTippers.xml")
+                @Storage("TipperDataContainer.xml")
         }
 )
-public class TipperDataManager implements PersistentStateComponent<TipperDataManager> {
+public class TipperDataManager implements PersistentStateComponent<TipperDataManager.TipperDataContainer> {
     @Nullable
     @Override
-    public TipperDataManager getState() {
-        return this;
+    public TipperDataContainer getState() {
+        return new TipperDataContainer();
     }
 
     @Override
-    public void loadState(TipperDataManager state) {
-        XmlSerializerUtil.copyBean(state, this);
+    public void loadState(TipperDataContainer state) {
+        // TODO
+    }
+
+    public class TipperDataContainer {
+        public Map<String, Object> data = new HashMap<>();
+
+        public TipperDataContainer() {
+            // TODO: Iterate over all tippers and get the data
+        }
     }
 }
