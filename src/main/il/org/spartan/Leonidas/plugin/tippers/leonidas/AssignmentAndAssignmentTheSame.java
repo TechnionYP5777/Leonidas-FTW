0589fb7ad5b8b646b87abe0224c039281d12654d
orenafek <oren.afek@gmail.com>
2017-06-29 18:39:45 +0300

    AssignmentAndAssignmentTheSame done.

---
+++ src/main/il/org/spartan/Leonidas/plugin/tippers/leonidas/AssignmentAndAssignmentTheSame.java
@@ -0,0 +1,60 @@
+package il.org.spartan.Leonidas.plugin.tippers.leonidas;
+
+import il.org.spartan.Leonidas.auxilary_layer.ExampleMapFactory;
+
+import java.util.Map;
+
+/**
+ * Inline assignment into subsequent assignment.
+ *
+ * @author Oren
+ * @since 29-06-2017
+ */
+public class AssignmentAndAssignmentTheSame implements LeonidasTipperDefinition {
+
+
+    @Override
+    public void constraints() {
+    }
+
+    X identifier0;
+
+    class X {
+
+        public X identifier1() {
+            return this;
+        }
+
+        public X identifier2() {
+            return this;
+        }
+    }
+
+    @Override
+    public void matcher() {
+        new Template(() -> {
+            /* start */
+            identifier0 = identifier0.identifier1();
+            identifier0 = identifier0.identifier2();
+            /* end */
+        });
+    }
+
+
+    @Override
+    public void replacer() {
+        new Template(() -> {
+            /* start */
+            identifier0 = identifier0.identifier1().identifier2();
+            /* end */
+        });
+    }
+
+    @Override
+    public Map<String, String> getExamples() {
+        return new ExampleMapFactory()
+                .put("s = s.trim();\n s = s.substring(0,1);", "s = s.trim().substring(0,1);")
+                .map();
+
+    }
+}
