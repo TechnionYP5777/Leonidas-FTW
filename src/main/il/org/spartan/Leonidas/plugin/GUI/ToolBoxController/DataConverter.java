24be6597e0a0e6de151904a18d0634cfc0f808a5
Sharon <sharonkl@campus.technion.ac.il>
2017-06-30 12:45:18 +0300

    Some design changes in the DataConverter

--- src/main/il/org/spartan/Leonidas/plugin/GUI/ToolBoxController/DataConverter.java
+++ src/main/il/org/spartan/Leonidas/plugin/GUI/ToolBoxController/DataConverter.java
@@ -1,19 +1,42 @@
 package il.org.spartan.Leonidas.plugin.GUI.ToolBoxController;
 
-import com.intellij.util.Function;
+import java.util.function.Function;
 
 /**
  * Created by Sharon on 30-Jun-17.
  */
 public class DataConverter<T> {
+    public static DataConverter<String> STRING = new DataConverter<>(s -> s, s -> s);
+
     private Function<T, String> toString;
     private Function<String, T> fromString;
 
-    public static String toString(Object obj) {
-        return ""; // TODO
+    public DataConverter(Function<T, String> toString, Function<String, T> fromString) {
+        this.toString = toString;
+        this.fromString = fromString;
     }
 
-    public static Object fromString(String s) {
-        return new Object(); // TODO
+    public String toString(T t) {
+        return toString.apply(t);
+    }
+
+    public Object fromString(String s) {
+        return fromString.apply(s);
+    }
+
+    public static String convert(Object o) {
+        if (o.getClass().isAssignableFrom(String.class)) {
+            return STRING.toString((String) o);
+        }
+
+        return "";
+    }
+
+    public static Object decode(String s, Class<?> c) {
+        if (c.isAssignableFrom(String.class)) {
+            return STRING.fromString(s);
+        }
+
+        return "";
     }
 }
