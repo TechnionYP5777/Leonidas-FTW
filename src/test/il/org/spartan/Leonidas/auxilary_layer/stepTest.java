97be0aa230788c565fbb76a18b60be6e09fa2aed
Sharon <sharonkl@campus.technion.ac.il>
2017-07-01 14:32:17 +0300

    Add tests for step and fix an issue

--- src/test/il/org/spartan/Leonidas/auxilary_layer/stepTest.java
+++ src/test/il/org/spartan/Leonidas/auxilary_layer/stepTest.java
@@ -1,5 +1,7 @@
 package il.org.spartan.Leonidas.auxilary_layer;
 
+import com.intellij.psi.PsiParameter;
+import com.intellij.psi.PsiType;
 import il.org.spartan.Leonidas.PsiTypeHelper;
 import org.junit.Assert;
 
@@ -20,5 +22,25 @@ public class stepTest extends PsiTypeHelper {
     public void testDocCommentC() {
         Assert.assertEquals(
 				step.docCommentString(createTestClassFromString("", "A", "/** javadoc */void foo(){}", "public")), "");
+    }
+
+    public void testFirstParameter() {
+        PsiParameter parameter = step.firstParameter(createTestMethodFromString("public void foo(int a, int b, int c) {}").getParameterList());
+
+        assertNotNull(parameter);
+        Assert.assertEquals(parameter.getType(), PsiType.INT);
+        Assert.assertEquals(parameter.getName(), "a");
+
+        Assert.assertNull(step.secondParameter(createTestMethodFromString("public void foo() {}").getParameterList()));
+    }
+
+    public void testSecondParameter() {
+        PsiParameter parameter = step.secondParameter(createTestMethodFromString("public void foo(int a, int b, int c) {}").getParameterList());
+
+        assertNotNull(parameter);
+        Assert.assertEquals(parameter.getType(), PsiType.INT);
+        Assert.assertEquals(parameter.getName(), "b");
+
+        Assert.assertNull(step.secondParameter(createTestMethodFromString("public void foo(int a) {}").getParameterList()));
     }
 }
